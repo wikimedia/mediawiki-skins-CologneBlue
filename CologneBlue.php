eccb91f91b24a8237ce1271b94ff24364d48faa9
jrobson <jrobson@wikimedia.org>
2014-03-20 15:02:49 -0700

    Import CologneBlue skin from MediaWiki core
    Removed from MediaWiki in I65768b4c29faf468.
    
    Change-Id: I9cc051f48074a033c684e5efa56a7b4dde51d366

---
+++ CologneBlue.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * CologneBlue skin
+ *
+ * @file
+ * @ingroup Extensions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+$wgExtensionCredits['skin'][] = array(
+	'path' => __FILE__,
+	'name' => 'SkinCologneBlue',
+	'url' => 'https://www.mediawiki.org/wiki/Skin:CologneBlue',
+);
+
+// Register files
+$wgAutoloadClasses['SkinCologneBlue'] = __DIR__ . '/SkinCologneBlue.php';
+$wgAutoloadClasses['CologneBlueTemplate'] = __DIR__ . '/SkinCologneBlue.php';
+$wgMessagesDirs['CologneBlue'] = __DIR__ . '/i18n';
+
+// Register skin
+$wgValidSkinNames['cologneblue'] = 'CologneBlue';
+
+// Register modules
+$wgResourceModules['skins.cologneblue'] = array(
+	'styles' => array(
+		'resources/screen.css' => array( 'media' => 'screen' ),
+		'resources/print.css' => array( 'media' => 'print' ),
+	),
+	'remoteExtPath' => 'CologneBlue',
+	'localBasePath' => __DIR__,
+);
+
