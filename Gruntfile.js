0af8ea0edf9eb5391b8bd769ab723d28ff04bf92
Umherirrender <umherirrender_de.wp@web.de>
2017-08-03 14:56:48 +0200

    build: Update banana and jsonlint
    Exclude composer vendor folder from jsonlint
    
    Change-Id: I3ad583ab6d8c4d9c12a741e73215595690fb8594

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,15 @@ module.exports = function ( grunt ) {
 			all: [
 				'**/*.json',
 				'.stylelintrc',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		stylelint: {
 			all: [
 				'**/*.css',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		}
 	} );
